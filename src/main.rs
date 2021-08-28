213ba08a1eea2126a14b0a18f29baa311776e0d3
SaurabhAkolkar <37813446+SaurabhAkolkar@users.noreply.github.com>
2021-08-28 19:10:16 +0530

    Congratulation your guessing game is working fine..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+   
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Something Goes Wrong..");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num)=>num,
+            Err(_)=>continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To Less"),
+            Ordering::Greater => println!("To Big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
